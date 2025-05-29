a201886682fe8281244f7609861156437ea60e98
khushi <kgandhi1127@gmail.com>
2025-05-30 01:46:35 +0530

    initial commit

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -7,9 +7,9 @@ const router = express.Router();
 // ✅ Routes
 router.get("/get-all", getTask);
 router.post("/upload", uploadTask);
-router.get("/get/:id", getTaskById);          // GET single task
+router.get("/get/:taskId", getTaskById);          // GET single task
 router.post("/assign", assignTaskToUser);     // POST assign
-router.put("/status/:id", updateTaskStatus);  // PUT status update
+router.put("/status/:taskId", updateTaskStatus);  // PUT status update
 router.get("/weekly/:userId", getWeeklyTasksForUser); // Weekly tasks
 router.post("/feedback", submitFeedback);     // POST feedback
 

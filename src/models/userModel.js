a7c5260a221ccf45a74cee4ea7a78bba0751ff3b
khushi <kgandhi1127@gmail.com>
2025-05-29 21:56:16 +0530

    initial commit

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,6 +6,7 @@ const initUserInheritance = async () => {
       CREATE TABLE IF NOT EXISTS users (
         userId VARCHAR(36) PRIMARY KEY,
         name VARCHAR(255),
+        dob DATE,
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         type ENUM('parent', 'child') NOT NULL

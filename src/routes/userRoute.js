f1f77ed895aaf17b6ba53c3d3d613352bf82e6e0
khushi <kgandhi1127@gmail.com>
2025-06-11 22:59:30 +0530

    initial commit

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -11,11 +11,11 @@ router.post("/create-parent", createParent);
 router.post("/create-child", createChild);
 router.put("/update-parent/:id", updateParent);
 router.put("/update-child/:id", updateChild);
-router.get("/parent/:userId/get-children", getChildren);
+router.get("/parent/:id/get-children", getChildren);
 
 // 📍 Location
-router.get("/:userId/get-location", getLocation);
-router.post("/:userId/update-location", updateLocation);
+router.get("/:id/get-location", getLocation);
+router.post("/:id/update-location", updateLocation);
 
 // 📤 All users (parent + child)
 router.get("/get-all-users", getUsers);

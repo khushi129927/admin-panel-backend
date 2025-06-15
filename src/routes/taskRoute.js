581469ec5eada34c3df0a878a884a809898d34b5
khushi <kgandhi1127@gmail.com>
2025-06-16 00:31:29 +0530

    initial commit

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,13 +1,13 @@
 // 📁 src/routes/taskRoute.js
 const express = require("express");
 const multer = require("multer");
-const { getTask, uploadTask, getTaskByWeek, assignTaskToUser, updateTaskStatus, getWeeklyTasksForUser, submitFeedback } = require("../controllers/taskController");
+const { getTask, uploadTask, getTasksByWeek, assignTaskToUser, updateTaskStatus, getWeeklyTasksForUser, submitFeedback } = require("../controllers/taskController");
 const router = express.Router();
 
 // ✅ Routes
 router.get("/get-all", getTask);
 router.post("/upload", uploadTask);
-router.get("/get/:week", getTaskByWeek);          // GET single task
+router.get("/get/:week", getTasksByWeek);          // GET single task
 router.post("/assign", assignTaskToUser);     // POST assign
 router.put("/status/:taskId", updateTaskStatus);  // PUT status update
 router.get("/weekly/:userId", getWeeklyTasksForUser); // Weekly tasks

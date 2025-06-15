cd56a6c9c6c45ff52abe1f6aa1152774a99687da
khushi <kgandhi1127@gmail.com>
2025-06-16 04:49:34 +0530

    initial commit

--- src/controllers/taskScoreController.js
+++ src/controllers/taskScoreController.js
@@ -6,7 +6,7 @@ exports.submitTaskScore = async (req, res) => {
 
   try {
     // 1. Fetch task from DB
-    const [rows] = await db.execute(`SELECT * FROM tasks WHERE taskId = ?`, [taskId]);
+    const [rows] = await db.execute(`SELECT * FROM task WHERE taskId = ?`, [taskId]);
     if (rows.length === 0) return res.status(404).json({ error: "Task not found." });
 
     const task = rows[0];

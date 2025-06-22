57f827fa6419400a03228ffeb6bcd84267d38688
khushi <kgandhi1127@gmail.com>
2025-06-22 14:13:51 +0530

    initial commit

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,7 +1,7 @@
 // 📁 src/routes/taskRoute.js
 const express = require("express");
 const multer = require("multer");
-const { getTask, uploadTask, getTasksByWeek, getTasksByTaskOwner, getCompletedTasksByOwnerAndChild, assignTaskToChild, updateTaskStatus, getWeeklyTasksForUser, submitFeedback } = require("../controllers/taskController");
+const { getTask, uploadTask, getTasksByWeek, getTasksByTaskOwner, getCompletedTasksByChildId, assignTaskToChild, updateTaskStatus, getWeeklyTasksForUser, submitFeedback } = require("../controllers/taskController");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 
@@ -10,7 +10,7 @@ router.get("/get-all", getTask);
 router.post("/upload", uploadTask);
 router.get("/get/week/:week", auth, getTasksByWeek);          // GET single task
 router.get("/:userId/:task_owner/:week", auth, getTasksByTaskOwner);
-router.get("/completed-tasks/:childId", auth, getCompletedTasksByOwnerAndChild);
+router.get("/completed-tasks/:childId", auth, getCompletedTasksByChildId);
 router.post("/assign", auth, assignTaskToChild);     // POST assign
 router.put("/status/:taskId", auth, updateTaskStatus);  // PUT status update
 router.get("/weekly/:userId", auth, getWeeklyTasksForUser); // Weekly tasks

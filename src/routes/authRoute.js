eb273d62c951f5aaad858a8a5d7add793243a882
khushi <kgandhi1127@gmail.com>
2025-06-22 10:32:42 +0530

    initial commit

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken, logoutUser } = require("../controllers/authController");
+const auth = require("../middleware/authMiddleware");
 
 // ✅ Verify JWT token
 router.get("/verify", verifyToken);
